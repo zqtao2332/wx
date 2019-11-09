dccd8426766e7d075252d09ac08f9ba24e5ad7b1
zqtao <453395742@qq.com>
2019-11-09 13:09:40 +0800

    微信签名验证

---
+++ src/main/java/me/zqt/wx/controller/WechatController.java
@@ -0,0 +1,103 @@
+package me.zqt.wx.controller;
+
+import me.zqt.wx.service.WechatMessageService;
+import me.zqt.wx.utils.SignUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @auther: zqtao
+ * @description: 微信请求核心处理，验证入口;验证服务号是否合法，以及消息转发
+ * @version: 1.0
+ */
+@RestController
+@RequestMapping("/wechat")
+public class WechatController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WechatController.class);
+    @Autowired
+    WechatMessageService wechatMessageService;
+
+    /**
+     * 微信服务号连接校验
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public void get(HttpServletRequest request, HttpServletResponse response) {
+        LOGGER.info("----------------开始处理微信签名校验------------------");
+        // 微信加密签名，signature结合了开发者填写的token参数和请求中的timestamp参数、nonce参数。
+        String signature = request.getParameter("signature");
+        // 时间戳
+        String timestamp = request.getParameter("timestamp");
+        // 随机数
+        String nonce = request.getParameter("nonce");
+        // 随机字符串
+        String echostr = request.getParameter("echostr");
+        LOGGER.info("signature is :" + signature
+                + "\ntimestamp is :" + timestamp
+                + "\nnonce is :" + nonce
+                + "\nechostr is :" + echostr);
+
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，否则接入失败
+            if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+                out.print(echostr);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.error(e.getMessage());
+        } finally {
+            out.close();
+            out = null;
+            LOGGER.info("----------------结束处理微信签名校验------------------");
+        }
+    }
+
+    /**
+     * 处理各种请求
+     * @param request
+     * @param response
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public void post(HttpServletRequest request, HttpServletResponse response) {
+        LOGGER.info("----------------开始处理微信发过来的消息------------------");
+        try {
+            // 微信服务器POST消息时用的是UTF-8编码，在接收时也要用同样的编码，否则中文会乱码；
+            request.setCharacterEncoding("UTF-8");
+            // 在响应消息（回复消息给用户）时，也将编码方式设置为UTF-8
+            response.setCharacterEncoding("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            LOGGER.error("编码错误");
+        }
+
+        // 调用核心业务类接收消息、处理消息
+        String respMessage = wechatMessageService.newMessageRequest(request);
+
+        // 响应消息
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            out.print(respMessage);
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.error(e.getMessage());
+        } finally {
+            out.close();
+            out = null;
+            LOGGER.info("----------------结束处理微信发过来的消息------------------");
+        }
+    }
+
+}
---
+++ src/main/java/me/zqt/wx/utils/WechatMessageUtil.java
@@ -0,0 +1,115 @@
+package me.zqt.wx.utils;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.core.util.QuickWriter;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.io.xml.PrettyPrintWriter;
+import com.thoughtworks.xstream.io.xml.XppDriver;
+import me.zqt.wx.model.message.ImageMessage;
+import me.zqt.wx.model.message.TextMessage;
+import me.zqt.wx.model.message.VoiceMessage;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @auther: zqtao
+ * @description: 消息工具类
+ * @version: 1.0
+ */
+public class WechatMessageUtil {
+    /**
+     * @param @param  request
+     * @param @return 微信XML 请求解析结果
+     * @param @throws Exception
+     * @Description: 解析微信发来的请求（XML）, 并将结果封装在 Map 中返回
+     */
+    public static Map<String, String> parseXml(HttpServletRequest request)
+            throws Exception {
+        // 将解析结果存储在HashMap中
+        Map<String, String> map = new HashMap<>();
+        // 从request中取得输入流
+        InputStream inputStream = request.getInputStream();
+        // 读取输入流
+        SAXReader reader = new SAXReader();
+        Document document = reader.read(inputStream);
+        // 得到xml根元素
+        Element root = document.getRootElement();
+        // 得到根元素的所有子节点
+        List<Element> elementList = root.elements();
+
+        // 遍历所有子节点
+        for (Element e : elementList)
+            map.put(e.getName(), e.getText());
+
+        // 释放资源
+        inputStream.close();
+        inputStream = null;
+
+        return map;
+    }
+
+    /**
+     * 对象到xml的处理
+     */
+    private static XStream xstream = new XStream(new XppDriver() {
+        public HierarchicalStreamWriter createWriter(Writer out) {
+            return new PrettyPrintWriter(out) {
+                // 对所有xml节点的转换都增加CDATA标记
+                // 微信   <ToUserName><![CDATA[toUser]]></ToUserName>
+                boolean cdata = true;
+
+                @SuppressWarnings("rawtypes")
+                public void startNode(String name, Class clazz) {
+                    super.startNode(name, clazz);
+                }
+
+                protected void writeText(QuickWriter writer, String text) {
+                    if (cdata) {
+                        writer.write("<![CDATA[");
+                        writer.write(text);
+                        writer.write("]]>");
+                    } else {
+                        writer.write(text);
+                    }
+                }
+            };
+        }
+    });
+
+    /**
+     * @param textMessage 文本消息对象
+     * @return xml
+     * @Description: 文本消息对象转换成xml
+     */
+    public static String textMessageToXml(TextMessage textMessage) {
+        xstream.alias("xml", textMessage.getClass());
+        return xstream.toXML(textMessage);
+    }
+
+    /**
+     * @param imageMessage
+     * @Description: 图片消息对象转换成xml
+     */
+    public static String imageMessageToXml(ImageMessage imageMessage) {
+        xstream.alias("xml", imageMessage.getClass());
+        return xstream.toXML(imageMessage);
+    }
+
+    /**
+     * @param @param voiceMessage
+     * @Description: 语音消息对象转换成xml
+     */
+    public static String voiceMessageToXml(VoiceMessage voiceMessage) {
+        xstream.alias("xml", voiceMessage.getClass());
+        return xstream.toXML(voiceMessage);
+    }
+
+}
