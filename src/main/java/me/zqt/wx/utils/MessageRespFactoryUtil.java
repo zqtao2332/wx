012dd9b3d7136605d5826caa324e6b885cad3e2e
zqtao <453395742@qq.com>
2019-11-09 13:47:40 +0800

    Create MessageRespFactoryUtil.java

---
+++ src/main/java/me/zqt/wx/utils/MessageRespFactoryUtil.java
@@ -0,0 +1,32 @@
+package me.zqt.wx.utils;
+
+import me.zqt.wx.model.message.BaseMessage;
+
+import java.util.Date;
+
+/**
+ * @auther: zqtao
+ * @description: 微信消息响应类型封装工具，泛型转换成需要的类型
+ * 抽取共有属性：String fromUserName, String toUserName, String msgType
+ * T extends BaseMessage
+ * @version: 1.0
+ */
+public class MessageRespFactoryUtil<T extends BaseMessage> {
+
+    /**
+     * @param messageModel
+     * @param fromUserName
+     * @param toUserName
+     * @param msgType
+     * @return 根据传递的 messageModel 对象的类型，封装共有属性，返回需要的对象实例
+     */
+    public T getInstance(T messageModel, String fromUserName, String toUserName, String msgType) {
+
+        messageModel.setToUserName(fromUserName);
+        messageModel.setFromUserName(toUserName);
+        messageModel.setCreateTime(new Date().getTime());
+        messageModel.setMsgType(msgType);
+
+        return (T) messageModel;
+    }
+}
