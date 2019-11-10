2321e95f03e8c29be3ed5826fcb88a567312bcce
zqtao <453395742@qq.com>
2019-11-10 23:08:45 +0800

    修正图片消息实体

---
+++ src/main/java/me/zqt/wx/model/message/req/ImageReqMessage.java
@@ -0,0 +1,17 @@
+package me.zqt.wx.model.message.req;
+
+import lombok.Data;
+import me.zqt.wx.model.ImageModel;
+
+/**
+ * @auther: zqtao
+ * @description: 请求消息之图片消息
+ * @version: 1.0
+ */
+@Data
+public class ImageReqMessage extends ReqBasicMessage {
+    /**
+     * 媒体文件ID
+     */
+    private String MediaId;
+}
---
+++ src/main/java/me/zqt/wx/model/message/resp/ImageRespMessage.java
@@ -0,0 +1,18 @@
+package me.zqt.wx.model.message.resp;
+
+import lombok.Data;
+import me.zqt.wx.model.ImageModel;
+import me.zqt.wx.model.message.BasicMessage;
+
+/**
+ * @auther: zqtao
+ * @description: 响应消息之图片消息
+ * @version: 1.0
+ */
+@Data
+public class ImageRespMessage extends BasicMessage {
+    /**
+     * 图片
+     */
+    private ImageModel Image;
+}
