5252447cd2b93b69463df26aacdf0e1eb72af745
zqtao <453395742@qq.com>
2019-11-11 10:21:14 +0800

    Update VideoModel.java

--- src/main/java/me/zqt/wx/model/VideoModel.java
+++ src/main/java/me/zqt/wx/model/VideoModel.java
@@ -13,8 +13,14 @@ public class VideoModel {
      * 通过素材管理中的接口上传多媒体文件，得到的id
      */
     private String MediaId;
+
     /**
-     * 视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数据
+     * 视频消息的标题
      */
-    private String ThumbMediaId;
+    private String Title;
+
+    /**
+     * 视频消息的描述
+     */
+    private String Description;
 }
