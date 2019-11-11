c9829d2cc9ff564500a39b344798effcccd3a5aa
zqtao <453395742@qq.com>
2019-11-11 19:17:58 +0800

    V1.0.0

---
+++ src/main/java/me/zqt/wx/constant/SignatureConstant.java
@@ -0,0 +1,19 @@
+package me.zqt.wx.constant;
+
+/**
+ * @auther: zqtao
+ * @description: 开发者个人隐私常量
+ * @version: 1.0
+ */
+public class SignatureConstant {
+    // 开发者自定义token
+    public static final String TOKEN = "zqt23token";
+    // 第三方用户唯一凭证
+//    public static final String APP_ID = "wx79c9med6fb314041";
+    // 第三方用户唯一凭证密钥
+//    public static final String APP_SECRET = "cb4cfuck554585fe44476142adecb972e";
+
+    // pc
+    public static final String APP_ID = "wx4fde9fuck53e";
+    public static final String APP_SECRET = "aa9f1d6f9fuck8c229f3b73c6782e787";
+}
