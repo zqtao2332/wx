222753f9dfec3fb0bce25e3c1bf3a200d93a9c64
zqtao <453395742@qq.com>
2019-11-15 23:26:29 +0800

    新增百度网盘链接数据库表

---
+++ src/main/java/me/zqt/wx/mapper/BaiduNetdiskMapper.java
@@ -0,0 +1,9 @@
+package me.zqt.wx.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import me.zqt.wx.model.entity.BaiduNetdisk;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface BaiduNetdiskMapper extends BaseMapper<BaiduNetdisk> {
+}
---
+++ src/main/java/me/zqt/wx/model/entity/BaiduNetdisk.java
@@ -0,0 +1,47 @@
+package me.zqt.wx.model.entity;
+
+import cn.hutool.core.date.DateTime;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @auther: zqtao
+ * @description: 百度网盘链接实体
+ * @version: 1.0
+ */
+@Data
+//@TableName("tb_baidu_netdisk") 已在yml 中配置了prefix = tb_
+public class BaiduNetdisk implements Serializable {
+
+    /**
+     * 主键
+     */
+    private Long id;
+    /**
+     * 资源名称
+     */
+    private String name;
+
+    /**
+     * 资源链接
+     */
+    private String url;
+
+    /**
+     * 有效期(永久有效/7/1天)
+     */
+    private Integer expiryDate;
+
+    /**
+     * 有效性(有/失效)
+     */
+    private Integer validity;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+}
