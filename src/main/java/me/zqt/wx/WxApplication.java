ddb493485b2f7aed40971a54892e644a1ad2a8d6
zqtao <453395742@qq.com>
2019-11-15 23:25:37 +0800

    新增mysql数据源

--- src/main/java/me/zqt/wx/WxApplication.java
+++ src/main/java/me/zqt/wx/WxApplication.java
@@ -1,9 +1,11 @@
 package me.zqt.wx;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("me.zqt.wx.controller.mapper")
 public class WxApplication {
 
     public static void main(String[] args) {
