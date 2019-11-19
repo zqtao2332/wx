7ee830fdf4f37af6a36022f48e8942a905003c04
zqtao <453395742@qq.com>
2019-11-19 20:41:59 +0800

    Update SignatureConstant.java

--- src/main/java/me/zqt/wx/constant/SignatureConstant.java
+++ src/main/java/me/zqt/wx/constant/SignatureConstant.java
@@ -9,11 +9,11 @@ public class SignatureConstant {
     // 开发者自定义token
     public static final String TOKEN = "zqt23token";
     // 第三方用户唯一凭证
-//    public static final String APP_ID = "wx79c9med6fb314041";
+//    public static final String APP_ID = "wx79c9ddd6fb314041";
     // 第三方用户唯一凭证密钥
-//    public static final String APP_SECRET = "cb4cfuck554585fe44476142adecb972e";
+//    public static final String APP_SECRET = "cb4c469554585fe44476142adecb972e";
 
     // pc
-    public static final String APP_ID = "wx4fde9fuck53e";
-    public static final String APP_SECRET = "aa9f1d6f9fuck8c229f3b73c6782e787";
+    public static final String APP_ID = "wx4fe40fde9a785b3e";
+    public static final String APP_SECRET = "aa9f1d6f984088c229f3b73c6782e787";
 }
